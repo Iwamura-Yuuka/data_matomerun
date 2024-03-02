b738fe85ffc4149ddab36e07d049b15ae45f9719
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2024-03-03 00:10:21 +0900

    add coco1

--- robomech2024/scripts/all.sh
+++ robomech2024/scripts/all.sh
@@ -7,16 +7,16 @@ sleep 30
 sleep 30
 
 ./all_no_steer50.sh
-sleep30
+sleep 30
 
 ./all_no_predict50.sh
-sleep30
+sleep 30
 
 ./all_expand50.sh
-sleep30
+sleep 30
 
 ./all_one_step50.sh
-sleep30
+sleep 30
 
 ./all_coco25.sh
 sleep 30
@@ -25,13 +25,31 @@ sleep 30
 sleep 30
 
 ./all_no_steer25.sh
-sleep30
+sleep 30
 
 ./all_no_predict25.sh
-sleep30
+sleep 30
 
 ./all_expand25.sh
-sleep30
+sleep 30
 
 ./all_one_step25.sh
+sleep 30
+
+./all_coco1.sh
+sleep 30
+
+./all_st1.sh
+sleep 30
+
+./all_no_steer1.sh
+sleep 30
+
+./all_no_predict1.sh
+sleep 30
+
+./all_expand1.sh
+sleep 30
+
+./all_one_step1.sh
 exit 0
---
+++ robomech2024/scripts/coco1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sleep 5
+roslaunch robomech2024 coco1.launch
+sleep 60
