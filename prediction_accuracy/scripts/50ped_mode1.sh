31c25cf706dc8dbdae8c47c5751b35dd70688068
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2024-07-16 11:33:26 +0900

    change param

--- prediction_accuracy/scripts/50ped_mode1.sh
+++ prediction_accuracy/scripts/50ped_mode1.sh
@@ -2,7 +2,7 @@
 
 for i in {1..10}
 do
-  for j in {0..24}
+  for j in {0..49}
   do
     roslaunch prediction_accuracy 50ped_mode1.launch play_bag_name:=$(printf "%d" $i) id:=$(printf "%d" $j)
   done
