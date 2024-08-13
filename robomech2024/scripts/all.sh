5fee3ff0e5fa787728545427ed345fea85a3a638
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2024-08-14 02:53:42 +0900

    commit for now

--- robomech2024/scripts/all.sh
+++ robomech2024/scripts/all.sh
@@ -9,14 +9,14 @@ sleep 30
 ./all_no_steer50.sh
 sleep 30
 
-./all_no_predict50.sh
-sleep 30
+# ./all_no_predict50.sh
+# sleep 30
 
-./all_expand50.sh
-sleep 30
+# ./all_expand50.sh
+# sleep 30
 
-./all_one_step50.sh
-sleep 30
+# ./all_one_step50.sh
+# sleep 30
 
 ./all_coco25.sh
 sleep 30
@@ -27,29 +27,29 @@ sleep 30
 ./all_no_steer25.sh
 sleep 30
 
-./all_no_predict25.sh
-sleep 30
+# ./all_no_predict25.sh
+# sleep 30
 
-./all_expand25.sh
-sleep 30
+# ./all_expand25.sh
+# sleep 30
 
-./all_one_step25.sh
-sleep 30
+# ./all_one_step25.sh
+# sleep 30
 
-./all_coco1.sh
-sleep 30
+# ./all_coco1.sh
+# sleep 30
 
-./all_st1.sh
-sleep 30
+# ./all_st1.sh
+# sleep 30
 
-./all_no_steer1.sh
-sleep 30
+# ./all_no_steer1.sh
+# sleep 30
 
-./all_no_predict1.sh
-sleep 30
+# ./all_no_predict1.sh
+# sleep 30
 
-./all_expand1.sh
-sleep 30
+# ./all_expand1.sh
+# sleep 30
 
-./all_one_step1.sh
+# ./all_one_step1.sh
 exit 0
